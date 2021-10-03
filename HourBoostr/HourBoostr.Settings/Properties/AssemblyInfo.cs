bb9127954e8555dc6a6c94f6c59a5d16652da2bc
(Fallox) Nikko Renolds <ni1kko@outlook.com>
2021-10-03 19:06:48 +0100

    Tweaks

--- HourBoostr/HourBoostr.Settings/Properties/AssemblyInfo.cs
+++ HourBoostr/HourBoostr.Settings/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("HourBoostr")]
-[assembly: AssemblyCopyright("Copyright © Ezzpify 2017")]
+[assembly: AssemblyCopyright("Copyright © Ezzpify & Ni1kko 2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
--- SingleBoostr/Steam4NET2/Properties/AssemblyInfo.cs
+++ SingleBoostr/Steam4NET2/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Steam4NET")]
-[assembly: AssemblyCopyright("Copyright ©  2010")]
+[assembly: AssemblyCopyright("Copyright © 2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
