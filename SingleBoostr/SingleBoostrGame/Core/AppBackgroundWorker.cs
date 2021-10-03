175314bbce5d1eecd2e539279c8622cb9bd2916f
(Fallox) Nikko Renolds <ni1kko@outlook.com>
2021-10-03 20:56:38 +0100

    New layout :)

---
+++ SingleBoostr/SingleBoostrGame/Core/AppBackgroundWorker.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading;
+
+namespace SingleBoostr.Game.Core
+{
+    internal class AppBackgroundWorker
+    {
+        private const int SecondsBetweenChecks = 5;
+        internal BackgroundWorker Instance;
+        internal AppBackgroundWorker()
+        {
+            Instance = new BackgroundWorker() { WorkerSupportsCancellation = true };
+            Instance.DoWork += DoWork;
+        }
+
+        private void DoWork(object sender, DoWorkEventArgs e)
+        {
+            var parentProcess = Process.GetProcesses().FirstOrDefault(o => o.Id == (int)e.Argument);
+
+            if (parentProcess == null)
+            {
+                Console.WriteLine($"Could not find parent ID. Exiting in {SecondsBetweenChecks} seconds.");
+                Thread.Sleep(TimeSpan.FromSeconds(SecondsBetweenChecks));
+            } else {
+                while (!Instance.CancellationPending)
+                {
+                    if (parentProcess.HasExited) break;
+                    Thread.Sleep(TimeSpan.FromSeconds(SecondsBetweenChecks));
+                }
+            }
+            Environment.Exit(1);
+        }
+    }
+}
---
+++ SingleBoostr/SingleBoostrGame/Objects/SteamApp.cs
@@ -0,0 +1,87 @@
+﻿using Steam4NET;
+using System;
+using System.Text;
+
+namespace SingleBoostrGame
+{
+    internal class SteamApp
+    {
+        private ISteamClient012 _steamClient012;
+        private ISteamApps001 _steamApps001;
+
+        internal uint ID = 0;
+        internal string Title = "Unknown game";
+        internal DateTime StartTime { get; set; }
+        internal string UpTime => (DateTime.Now - StartTime).ToString().Split(new[] { '.' })[0];
+        private bool _running = false;
+        internal bool Running
+        {
+            get => _running;
+            private set
+            {
+                if (value) StartTime = DateTime.Now;
+                Title = value ? ToString() : "Unknown game";
+                _running = value;
+            }
+        }
+        internal SteamApp(uint appID)
+        {
+            ID = appID;
+            Environment.SetEnvironmentVariable("SteamAppId", ID.ToString());
+        }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder(128);
+            _steamApps001.GetAppData(ID, "name", sb);
+            string gameName = sb.ToString().Trim();
+            return string.IsNullOrWhiteSpace(gameName) ? "Unknown game" : Encoding.UTF8.GetString(Encoding.Default.GetBytes(gameName));
+        }
+
+        internal bool Connect()
+        {
+            void ShowError(string str)
+            {
+                Console.WriteLine($"ERROR: {str}\n\nPress any key to exit...");
+                Console.ReadKey();
+            }
+
+            if (!Steamworks.Load(true))
+            {
+                ShowError("Steamworks failed to load.");
+                return false;
+            }
+
+            _steamClient012 = Steamworks.CreateInterface<ISteamClient012>();
+            if (_steamClient012 == null)
+            {
+                ShowError("Failed to create Steam Client inferface.");
+                return false;
+            }
+
+            int pipe = _steamClient012.CreateSteamPipe();
+            if (pipe == 0)
+            {
+                ShowError("Failed to create Steam pipe.");
+                return false;
+            }
+
+            int user = _steamClient012.ConnectToGlobalUser(pipe);
+            if (user == 0)
+            {
+                ShowError("Failed to connect to Steam user.");
+                return false;
+            }
+
+            _steamApps001 = _steamClient012.GetISteamApps<ISteamApps001>(user, pipe);
+            if (_steamApps001 == null)
+            {
+                ShowError("Failed to create Steam Apps inferface.");
+                return false;
+            }
+            else Running = true;
+
+            return Running;
+        }
+    } 
+}
