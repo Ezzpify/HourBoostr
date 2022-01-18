375f8a329dccdb1430807faeb7ca0b546f8cd632
(Fallox) Nikko Renolds <ni1kko@outlook.com>
2022-01-18 22:34:19 +0000

    Updated `SplashScreen`

--- Source/HourBoostr-Beta/Ui/SplashScreen.Designer.cs
+++ Source/HourBoostr-Beta/Ui/SplashScreen.Designer.cs
@@ -28,13 +28,58 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.InfoBox = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::HourBoostr_Beta.Properties.Resources.SplashScreen;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 1);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(870, 481);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // InfoBox
+            // 
+            this.InfoBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(58)))), ((int)(((byte)(58)))), ((int)(((byte)(58)))));
+            this.InfoBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.InfoBox.Enabled = false;
+            this.InfoBox.Font = new System.Drawing.Font("Montserrat", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.InfoBox.ForeColor = System.Drawing.SystemColors.Window;
+            this.InfoBox.Location = new System.Drawing.Point(0, 425);
+            this.InfoBox.Multiline = true;
+            this.InfoBox.Name = "InfoBox";
+            this.InfoBox.Size = new System.Drawing.Size(870, 26);
+            this.InfoBox.TabIndex = 1;
+            this.InfoBox.Text = "Loading...  Please wait!";
+            this.InfoBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // SplashScreen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(58)))), ((int)(((byte)(58)))), ((int)(((byte)(58)))));
+            this.ClientSize = new System.Drawing.Size(870, 480);
+            this.ControlBox = false;
+            this.Controls.Add(this.InfoBox);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "SplashScreen";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        internal System.Windows.Forms.TextBox InfoBox;
     }
 }
 
