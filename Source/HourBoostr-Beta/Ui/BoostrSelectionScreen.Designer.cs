fadc4cf982b47ba8fd274281e06fd5a9c1e06a1a
(Fallox) Nikko Renolds <ni1kko@outlook.com>
2022-01-18 22:44:20 +0000

    Updated `BoostrSelectionScreen`

--- Source/HourBoostr-Beta/Ui/BoostrSelectionScreen.Designer.cs
+++ Source/HourBoostr-Beta/Ui/BoostrSelectionScreen.Designer.cs
@@ -28,10 +28,22 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(BoostrSelectionScreen));
+            this.SuspendLayout();
+            // 
+            // BoostrSelectionScreen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "BoostrSelectionScreen";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "BoostrSelectionScreen";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
