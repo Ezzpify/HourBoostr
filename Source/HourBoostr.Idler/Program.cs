1a1ffdbcfef84e00995c760b5af0ffaf00c9f8a0
(Fallox) Nikko Renolds <ni1kko@outlook.com>
2022-01-21 15:37:42 +0000

    Update Program.cs

--- Source/HourBoostr.Idler/Program.cs
+++ Source/HourBoostr.Idler/Program.cs
@@ -16,9 +16,9 @@ namespace HourBoostr.Idler
     {
         internal static Program This { get; private set; }
         internal ProgramAssembly Assembly { get; init; }
-        internal ProgramArguments Arguments { get; init; }  
-        internal ISteamClient SteamClient { get; private set; }
-        internal ISteamApps SteamApps { get; private set; }
+        internal ProgramArguments Arguments { get; init; }
+        internal ISteamClient SteamClient { get; set; }
+        internal ISteamApps SteamApps { get; set; }
         private int User { get; set; } = 0;
         private int Pipe { get; set; } = 0;
         
@@ -27,8 +27,6 @@ namespace HourBoostr.Idler
         private uint AppID { get; init; }
 
         private static IntPtr SteamClientHandle = IntPtr.Zero;
-        private static IntPtr SteamHandle = IntPtr.Zero;
-
         private struct Native
         {
             [DllImport("kernel32.dll", SetLastError = true)]
@@ -41,10 +39,7 @@ namespace HourBoostr.Idler
             internal static extern IntPtr SetDllDirectory(string lpPathName);
 
             internal const UInt32 LOAD_WITH_ALTERED_SEARCH_PATH = 8;
-
-            [UnmanagedFunctionPointer(CallingConvention.StdCall, CharSet = CharSet.Ansi)]
-            internal delegate IntPtr _f(string version);
-
+             
             [UnmanagedFunctionPointer(CallingConvention.StdCall, CharSet = CharSet.Ansi)]
             internal delegate IntPtr CreateInterface(string version, IntPtr returnCode);
 
@@ -59,8 +54,7 @@ namespace HourBoostr.Idler
                 return (TDelegate)(object)Marshal.GetDelegateForFunctionPointer(address, typeof(TDelegate));
             }
         }
-        private static Native.CreateInterface CallCreateInterface;
-        private static Native._f CallCreateSteamInterface;
+        private static Native.CreateInterface CallCreateSteamClientInterface;
 
         internal Program(List<string> arguments)
         {
@@ -99,17 +93,50 @@ namespace HourBoostr.Idler
             {
                 static async Task<bool> Connect()
                 {
-                    if (!This.Load(true)) return false;
-                    if (Environment.CurrentDirectory == This.GetInstallPath()) return false;
-                     
-                    This.SteamClient = This.CreateInterface<ISteamClient>();
+                    static async Task<bool> InitSteamLibs()
+                    {
+                        string BoostrPath = Environment.CurrentDirectory;
+                        string SteamPath = Path.GetFullPath((string)Registry.GetValue(@"HKEY_CURRENT_USER\SOFTWARE\Valve\Steam", "SteamPath", null));
+
+                        static async Task<bool> LoadSteamClientLib(string path)
+                        {
+                            if (string.IsNullOrEmpty(path) || !Directory.Exists(path)) return false;
+                            if (SteamClientHandle != IntPtr.Zero) return true;
+                            Native.SetDllDirectory(path + ";" + Path.Combine(path, "bin"));
+                            path = Environment.Is64BitProcess ? Path.Combine(path, "steamclient64.dll") : Path.Combine(path, "steamclient.dll");
+
+                            IntPtr module = Native.LoadLibraryEx(path, IntPtr.Zero, Native.LOAD_WITH_ALTERED_SEARCH_PATH);
+                            if (module == IntPtr.Zero) return false;
+                            await Task.Delay(500);
+
+                            CallCreateSteamClientInterface = Native.GetExportFunction<Native.CreateInterface>(module, "CreateInterface");
+                            if (CallCreateSteamClientInterface == null) return false;
+
+                            SteamClientHandle = module;
+                            return true;
+                        }
+
+                        return !SteamPath.Equals(BoostrPath) && await LoadSteamClientLib(SteamPath);
+                    }
+
+                    static TClass CreateSteamClientInterface<TClass>() where TClass : InteropHelp.INativeWrapper, new()
+                    {
+                        if (CallCreateSteamClientInterface == null) throw new InvalidOperationException($"steamclient{(Environment.Is64BitProcess ? "64" : "")}.dll has not been initialized");
+                        IntPtr address = CallCreateSteamClientInterface(Utils.GetInterfaceIdentifier(typeof(TClass)), IntPtr.Zero);
+                        if (address == IntPtr.Zero) return default;
+                        var rez = new TClass();
+                        rez.SetupFunctions(address);
+                        return rez;
+                    }
+
+                    if (!await InitSteamLibs()) return false;
+
+                    This.SteamClient = CreateSteamClientInterface<ISteamClient>();
                     if (This.SteamClient == null) return false;
                      
                     This.Pipe = This.SteamClient.CreateSteamPipe();
                     if (This.Pipe == 0) return false;
-
-                    await Task.Delay(500);
-
+                     
                     This.User = This.SteamClient.ConnectToGlobalUser(This.Pipe);
                     if (This.User == 0 || This.User == -1) return false;
                      
@@ -136,94 +163,8 @@ namespace HourBoostr.Idler
 
                     //Kill App
                     Environment.Exit(-1);
-
                 }
             }
-        }
-        
-        public TClass CreateInterface<TClass>() where TClass : InteropHelp.INativeWrapper, new()
-        {
-            if (CallCreateInterface == null)
-                throw new InvalidOperationException("Steam4NET library has not been initialized.");
-
-            IntPtr address = CallCreateInterface(Utils.GetInterfaceIdentifier(typeof(TClass)), IntPtr.Zero);
-
-            if (address == IntPtr.Zero)
-                return default;
-
-            var rez = new TClass();
-            rez.SetupFunctions(address);
-
-            return rez;
-        }
-        public TClass CreateSteamInterface<TClass>() where TClass : InteropHelp.INativeWrapper, new()
-        {
-            if (CallCreateSteamInterface == null)
-                throw new InvalidOperationException("Steam4NET library has not been initialized.");
-
-            IntPtr address = CallCreateSteamInterface(Utils.GetInterfaceIdentifier(typeof(TClass)));
-
-            if (address == IntPtr.Zero)
-                return default;
-
-            var rez = new TClass();
-            rez.SetupFunctions(address);
-
-            return rez;
-        }
-
-        private string GetInstallPath() => Path.GetFullPath((string)Registry.GetValue(@"HKEY_CURRENT_USER\SOFTWARE\Valve\Steam", "SteamPath", null));
-        private bool Load(bool steam) => steam && !LoadSteam() ? false : LoadSteamClient();
-        private bool LoadSteam()
-        {
-            if (SteamHandle != IntPtr.Zero)
-                return true;
-
-            string path = GetInstallPath();
-            if (!string.IsNullOrEmpty(path))
-                Native.SetDllDirectory(path + ";" + Path.Combine(path, "bin"));
-
-            path = Path.Combine(path, "steam.dll");
-
-            IntPtr module = Native.LoadLibraryEx(path, IntPtr.Zero, Native.LOAD_WITH_ALTERED_SEARCH_PATH);
-
-            if (module == IntPtr.Zero)
-                return false;
-
-            CallCreateSteamInterface = Native.GetExportFunction<Native._f>(module, "_f");
-
-            if (CallCreateSteamInterface == null)
-                return false;
-
-            SteamHandle = module;
-
-            return true;
-        }
-        private bool LoadSteamClient()
-        {
-            if (SteamClientHandle != IntPtr.Zero)
-                return true;
-
-            string path = GetInstallPath();
-
-            if (!string.IsNullOrEmpty(path))
-                Native.SetDllDirectory(path + ";" + Path.Combine(path, "bin"));
-            if (Environment.Is64BitProcess)
-                path = Path.Combine(path, "steamclient64.dll");
-            else
-                path = Path.Combine(path, "steamclient.dll");
-            IntPtr module = Native.LoadLibraryEx(path, IntPtr.Zero, Native.LOAD_WITH_ALTERED_SEARCH_PATH);
-
-            if (module == IntPtr.Zero)
-                return false;
-
-            CallCreateInterface = Native.GetExportFunction<Native.CreateInterface>(module, "CreateInterface");
-            if (CallCreateInterface == null)
-                return false;
-
-            SteamClientHandle = module;
-
-            return true;
         }
     }
 }
