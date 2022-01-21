6b1e2335fb19792e7efb202eafea24addc7e9e7f
(Fallox) Nikko Renolds <ni1kko@outlook.com>
2022-01-21 15:37:17 +0000

    Update ProgramArguments.cs

--- Source/HourBoostr.Idler/Core/ProgramArguments.cs
+++ Source/HourBoostr.Idler/Core/ProgramArguments.cs
@@ -5,8 +5,10 @@ namespace HourBoostr.Idler.Core
 {
     internal class ProgramArguments
     {
+        private readonly string Param1Spliter = "-";
         internal bool MultiBoostr { get; init; }
         internal string AppID { get; init; }
+        internal int ParentProcessID { get; init; } //property is Immutable and cannot be changed once object is created
 
         internal ProgramArguments(List<string> arguments)
         {
@@ -15,16 +17,27 @@ namespace HourBoostr.Idler.Core
                 var index = 0;
                 foreach (var arg in arguments)
                 {
-                    if (!arg.StartsWith("-")) continue;
+                    if (!arg.StartsWith(Param1Spliter)) continue;
+                     
+                    var indexparam = index;
 
-                    switch (arg.ToLower().Replace("-", ""))
+                    //argument 1
+                    switch (arg.ToLower().Replace(Param1Spliter, ""))
                     {
                         case "multi": MultiBoostr = true; continue;
                         case "single":
-                            if (arguments.Count == 2)
-                            {
-                                AppID = arguments[index + 1];
-                            }
+                            if (arguments.Count < 2) continue;
+
+                            //argument 2
+                            indexparam++;
+                            AppID = arguments[indexparam];
+                            if (arguments.Count < 3) continue;
+
+                            //argument 3
+                            indexparam++;
+                            if (int.TryParse(arguments[indexparam], out int parentProcessID))
+                                ParentProcessID = parentProcessID;
+
                             continue;
                     }
 
